e86eec2492ea2d401cce5388d5e9b14847eac691
LuoMuyu <47489339+LuoMuyu@users.noreply.github.com>
2022-05-05 15:55:36 +0800

    Add files via upload

---
+++ dist/assets/useTitle.f437284d.js
@@ -0,0 +1,1 @@
+import{r as o}from"./vendor.2bf3c6ec.js";const r=(e,t=!1)=>{o.exports.useEffect(()=>{const s=document.title;return t?document.title=`${e} - alist`:document.title=e,()=>{document.title=s}},[])};var u=r;export{u};
